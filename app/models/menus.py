abe16594cc056ea0a8a80090f2107ac034739f0a
Johnny R Paredez III <jparedeziii@Mac.lan>
2025-02-28 15:39:24 -0600

    renamed description to name for menu model

--- app/models/menus.py
+++ app/models/menus.py
@@ -7,6 +7,6 @@ class Menu(db.Model):
     __tablename__ = 'menus'
     
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
     
     items = db.relationship('Item', back_populates='menu', cascade='all, delete-orphan')
