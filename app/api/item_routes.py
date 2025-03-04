46a397c0982e474a884ff618c605c7657318a307
Johnny R Paredez III <jparedeziii@Mac.lan>
2025-03-04 13:36:17 -0600

    put the route back lol

--- app/api/item_routes.py
+++ app/api/item_routes.py
@@ -1,7 +1,7 @@
 # cap/app/api/item_routes.py
 from flask import Blueprint, request, jsonify
 from flask_login import login_required
-from app.models import db, Item, Order
+from app.models import db, Item
 
 item_routes = Blueprint('items', __name__)
 
@@ -12,32 +12,6 @@ def get_items():
 
     items = Item.query.all()
     return jsonify([item.to_dict() for item in items]), 200
-
-# POST: Add items to an existing order
-@item_routes.route('/<int:id>', methods=['POST'])
-@login_required
-def add_to_order(id):
-    order = Order.query.get(id)
-    if not order:
-        return jsonify({"error": "Order not found"}), 404
-
-    data = request.get_json()
-
-    # Validate input
-    if "item_ids" not in data or not isinstance(data["item_ids"], list):
-        return jsonify({"error": "item_ids must be a list of item IDs"}), 400
-
-    # Query items based on the list of item_ids
-    new_items = Item.query.filter(Item.id.in_(data["item_ids"])).all()
-
-    if not new_items:
-        return jsonify({"error": "No valid items found"}), 400
-
-    # Add new items to the existing order
-    order.items.extend(new_items)
-    db.session.commit()
-
-    return jsonify(order.to_dict()), 200
 
 # POST: Add a new item to a menu category
 @item_routes.route('/', methods=['POST'])
