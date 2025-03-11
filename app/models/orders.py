e5405ee93ca4de6d3eed59869b099eae711302c5
Johnny R Paredez III <jparedeziii@Mac.lan>
2025-03-11 12:47:29 -0500

    fix database issues

--- app/models/orders.py
+++ app/models/orders.py
@@ -5,12 +5,15 @@ from .items import order_items, Item
 class Order(db.Model):
     __tablename__ = "orders"
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     krustomer_name = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), nullable=False, default="pending")
     
     # Define the relationship with Order Items
-    items = db.relationship("Item", secondary="order_items", back_populates="orders")
+    items = db.relationship("Item", secondary=order_items, back_populates="orders")
 
     def to_dict(self):
         return {
@@ -24,9 +27,10 @@ class Order(db.Model):
                     "price": item.price,
                     "category": item.category,
                     "is_active": item.is_active,
-                    "quantity": order_item.quantity
+                    "quantity": db.session.query(order_items.c.quantity)
+                                         .filter(order_items.c.order_id == self.id, order_items.c.item_id == item.id)
+                                         .scalar()
                 }
                 for item in self.items
-                for order_item in db.session.query(order_items).filter_by(order_id=self.id, item_id=item.id)
             ]
         }
