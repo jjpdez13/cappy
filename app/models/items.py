5d6078c2acea7738a1543a7fdd1ae5f2d561692b
Johnny R Paredez III <jparedeziii@Mac.lan>
2025-03-13 10:59:50 -0500

    updated items.py and added migration for item name length

--- app/models/items.py
+++ app/models/items.py
@@ -31,7 +31,7 @@ class Item(db.Model):
         
     id = db.Column(db.Integer, primary_key=True)
     menu_id = db.Column(db.Integer, db.ForeignKey(f'{SCHEMA}.menus.id') if environment == "production" else db.ForeignKey("menus.id"), nullable=False)
-    name = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(255), nullable=False)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
